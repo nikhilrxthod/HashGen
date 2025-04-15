13124606bfc6ee7af02a53caddf12c689fb34751
Nikhil Rathod <103273630+nikhilrxthod@users.noreply.github.com>
2025-04-15 12:25:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,52 +12,30 @@ def index():
         
         if algorithm == "md5":
             hash_result = hashlib.md5(text.encode()).hexdigest()
-
         elif algorithm == "sha256":
             hash_result = hashlib.sha256(text.encode()).hexdigest()
-
         elif algorithm == "sha1":
             hash_result = hashlib.sha1(text.encode()).hexdigest()
-
-        
-
         elif algorithm == "sha384":
             hash_result = hashlib.sha384(text.encode()).hexdigest()
-
         elif algorithm == "sha512":
             hash_result = hashlib.sha512(text.encode()).hexdigest()
-
-      
-
         elif algorithm == "sha3_512":
             hash_result = hashlib.sha3_512(text.encode()).hexdigest()
-
         elif algorithm == "blake2s":
             hash_result = hashlib.blake2s(text.encode()).hexdigest()
-
         elif algorithm == "sha224":
             hash_result = hashlib.sha224(text.encode()).hexdigest()
-
-       
-
         elif algorithm == "shake_128":
             hash_result = hashlib.shake_128(text.encode()).hexdigest()
-
         elif algorithm == "sha3_256":
             hash_result = hashlib.sha3_256(text.encode()).hexdigest()
-
         elif algorithm == "blake2b":
             hash_result = hashlib.blake2b(text.encode()).hexdigest()
-
         elif algorithm == "sha3_384":
             hash_result = hashlib.sha3_384(text.encode()).hexdigest()
-
         elif algorithm == "sha3_224":
             hash_result = hashlib.sha3_224(text.encode()).hexdigest()
-
- 
-
-      
 
     return render_template("index.html", hash_result=hash_result)
 
